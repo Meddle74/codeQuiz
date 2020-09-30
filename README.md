83f462b7a32f4b4f0f33005e1a050935ae1483e8
Patrick <patrick.lloyd@theproducebox.com>
2020-09-29 23:13:27 -0400

    Cleaned up javascript file to make it more orgainzed and added section notation. Finished leaderboard storage and clear. Final updates to code.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# codeQuiz
+# Code Quiz
+
+## General info
+Create a timed quiz on Javascript that stores high scores so that you can gauge your progress against your peers.
+
+![Algorithm schema](quiz.png)
+
+## Features
+
+* Timer starts to countdown once quiz is started.
+* When incorrect answer is given you are penalized 10 seconds on the timer.
+* The game is over when:
+  * Timer runs out.
+  * All questinons are answered.
+* When the game is over you can save your name and score to the leaderboard.
+* When viewing the leaderboard you can clear all scores.
+
+	
+## Technologies
+Project is created with:
+* Javascript
+* CSS
+* HTML
+
+## Link to deployed application 
+
+[Code Quiz](https://www.google.com)
--- script.js
+++ script.js
@@ -1,123 +1,16 @@
-
+// elements
 var questionArea = document.querySelector('#questionArea');
 var answerRow = document.querySelector('#answerRow');
 var answerResult = document.querySelector('#answerResult');
-
-
 var createAnswersDiv = document.createElement('div');
-
 var startQuizBtn = document.querySelector('#start');
-var nextQuestionBtn = document.querySelector('#next');
-
-var numberCorrect = 0;
-var numberWrong = 0;
-var scores = [];
-
-// Timer setup
-var quizTimerLength = 45;
 var timerElement = document.querySelector('.timer');
-timerElement.innerHTML = `<span>${quizTimerLength} </span>`;
+var viewLeaderBoardBtn = document.querySelector('.viewLeaderboard');
 
-function startTimer() {
-  var countdownTimer = setInterval(function () {
-    if (quizTimerLength <= 0) {
-      clearInterval(countdownTimer);
-    }
-    timerElement.innerHTML = `<span>${quizTimerLength} </span>`;
-    quizTimerLength -= 1;
-  }, 1000);
-}
-
-function displayTimer(element, seconds) {
-  // Reset Opacity on element
-  element.style = 'opacity: 1;';
-
-  var displayLength = seconds;
-  var countdownTimer = setInterval(function () {
-    if (displayLength <= 0) {
-      element.style = 'opacity: 0;';
-      clearInterval(countdownTimer);
-    }
-    displayLength -= 1;
-  }, 1000);
-}
-
-function leaderBoard() {
-  // Grab container and clear
-  var container = document.querySelector('.container');
-  container.innerHTML = '';
-
-  var highscores = document.querySelector('.viewLeaderboard');
-  highscores.innerHTML = '';
-  
-  // If there are scores in localStorage read them into scores
-  readFromLocalStorage();
-
-  // Create leaderboard title
-  var title = document.createElement('h1');
-  title.classList.add('my-3');
-  title.innerText = 'Leaderboard';
-  container.appendChild(title);
-  var list = document.createElement('ol');
-  list.style = 'width: 10rem; margin: 0 auto; text-align: left;';
-
-  var score = {
-    name: 'Patrick',
-    score: 999,
-  };
-
-  scores.push(score);
-  console.log(scores);
-  var sortedScores = scores.sort(function (a, b) {
-    console.log(a.score, b.score);
-    return b.score - a.score;
-  });
-  sortedScores.forEach((scoreEntry) => (list.innerHTML += `<li>${scoreEntry.name} - ${scoreEntry.score}</li>`));
-
-  container.appendChild(list);
-
-  // Add Return Button
-  var returnButton = document.createElement('button');
-  returnButton.innerText = 'Return to Main';
-  returnButton.classList.add('mt-3', 'btn', 'btn-success');
-  container.appendChild(returnButton);
-  
-  // Return To Main Page
-  returnButton.addEventListener('click', function(){
-    window.location.href ='index.html'
-  })
-
-  //Add Clear Scores Button
-  var clearButton = document.createElement('button');
-  clearButton.innerText = 'Clear Scores';
-  clearButton.classList.add('mt-3', 'ml-3', 'btn', 'btn-danger');
-  container.appendChild(clearButton);
-
-  //Reload Page
-  clearButton.addEventListener('click', function(){
-    localStorage.removeItem('scores');
-    window.location.reload() // need to fix this to not go to main page just to refresh with empty array?
-  })
-  
-
-  saveToLocalStorage(sortedScores);
-}
-
-function readFromLocalStorage() {
-  console.info('Retrieveing from local storage');
-  var localStoreScores = JSON.parse(localStorage.getItem('scores'));
-  console.log(Array.isArray(localStoreScores));
-  if (localStoreScores) {
-    scores.push(...localStoreScores);
-  }
-}
-
-function saveToLocalStorage(scoresArray) {
-  console.info('Saving scores to local storage');
-  localStorage.setItem('scores', JSON.stringify(scoresArray));
-}
-
-// Questions for Quiz
+// variables
+var numberCorrect = 0;
+var scores = [];
+var quizTimerLength = 45;
 var questions = [
   {
     text: 'Inside which HTML element do we put the JavaScript? _______',
@@ -133,7 +26,7 @@ var questions = [
     answers: [
       { text: '1.  The <head> section', value: false },
       { text: '2.  The <body> section', value: true },
-      { text: '3.  Bot the <head> and the <body> section', value: false },
+      { text: '3.  Both the <head> and the <body> section', value: false },
     ],
   },
   {
@@ -154,23 +47,195 @@ var questions = [
     ],
   },
 ];
-
 var questionNum = 0;
-var quizLength = questions.length - 1;
+var quizLength = questions.length;
 
+// Timer Section ---------------------------------------------------------
+timerElement.innerHTML = `<span>${quizTimerLength} </span>`;
+
+function startTimer() {
+  var countdownTimer = setInterval(function () {
+    if (quizTimerLength <= 0) {
+      clearInterval(countdownTimer);
+      enterNameIntoLeaderboard();
+    }
+    timerElement.innerHTML = `<span>${quizTimerLength} </span>`;
+    quizTimerLength -= 1;
+  }, 1000);
+}
+
+function displayTimer(element, seconds) {
+  // Reset Opacity on element
+  element.style = 'opacity: 1;';
+
+  var displayLength = seconds;
+  var countdownTimer = setInterval(function () {
+    if (displayLength <= 0) {
+      element.style = 'opacity: 0;';
+      clearInterval(countdownTimer);
+    }
+    displayLength -= 1;
+  }, 1000);
+}
+// End Timer Section -----------------------------------------------------
+
+// Leaderboard Section ---------------------------------------------------
+function leaderBoard() {
+  // Grab container and clear
+  var container = document.querySelector('.container');
+  container.innerHTML = '';
+
+  timerElement.style = 'display: none;';
+
+  var highscores = document.querySelector('.viewLeaderboard');
+  highscores.innerHTML = '';
+  
+  // Populate scores array from local storage
+  readFromLocalStorage();
+
+  // Create leaderboard title
+  var title = document.createElement('h1');
+  title.classList.add('my-3');
+  title.innerText = 'Leaderboard';
+  container.appendChild(title);
+
+  // Create ordered list for highscores
+  var list = document.createElement('ol');
+  list.style = 'width: 10rem; margin: 0 auto; text-align: left;';
+
+  // Sort scores
+  if (scores) {
+    var sortedScores = scores.sort(function (a, b) {
+      return b.score - a.score;
+    });
+    sortedScores.forEach((scoreEntry) => (list.innerHTML += `<li>${scoreEntry.name} - ${scoreEntry.score}%</li>`));
+  }
+
+  // Add ordered list to page
+  container.appendChild(list);
+
+  // Add Return Button
+  var returnButton = document.createElement('button');
+  returnButton.innerText = 'Return to Main';
+  returnButton.classList.add('mt-3', 'btn', 'btn-success');
+  container.appendChild(returnButton);
+  
+  // Return To Main Page
+  returnButton.addEventListener('click', function(){
+    window.location.href ='index.html'
+  })
+
+  // Add Clear Scores Button
+  var clearButton = document.createElement('button');
+  clearButton.innerText = 'Clear Scores';
+  clearButton.classList.add('mt-3', 'ml-3', 'btn', 'btn-danger');
+  container.appendChild(clearButton);
+
+  // Refresh Page
+  clearButton.addEventListener('click', function(){
+    localStorage.removeItem('scores');
+
+    // Clear scores array
+    scores.length = 0;
+    
+    // Call leaderboard function to refresh leaderboard
+    leaderBoard();
+  })
+}
+// End Leaderboard Section -----------------------------------------------
+
+// Utility Functions Section ---------------------------------------------
+function calcScore(correct, length) {
+  return correct / length * 100; 
+}
+
+function buildScoreObj(name, score) {
+  var obj = {}
+  obj.name = name;
+  obj.score = score;
+  
+  return obj;
+}
+
+function nextQuestion() {
+  // Grab Answer Area
+  var answerArea = document.getElementById('answersArea');
+
+  // Remove previous answers
+  answerArea.innerHTML = '';
+}
+// End Utility Functions Section -----------------------------------------
+
+// Add Name To Leaderboard Section ---------------------------------------
+function enterNameIntoLeaderboard() {
+  // Grab container and clear
+  var container = document.querySelector('.container');
+  container.innerHTML = '';
+
+  // Hide Timer Element
+  timerElement.style = 'display: none;';
+  
+  var highscores = document.querySelector('.viewLeaderboard');
+  highscores.innerHTML = '';
+  
+  // Populate scores array from local storage
+  readFromLocalStorage();
+  
+  // Create leaderboard entry page
+  var title = document.createElement('h1');
+  title.classList.add('my-3');
+  title.innerText = 'Enter Name for Leaderboard';
+  container.appendChild(title);
+  var scoreField = document.createElement('p');
+  var score = calcScore(numberCorrect, quizLength).toFixed(2);
+  scoreField.innerText = `Score : ${ score }%`;
+  container.appendChild(scoreField);
+
+  var nameInput = document.createElement('input');
+  container.appendChild(nameInput);
+  
+  var submitNameBtn = document.createElement('button');
+  submitNameBtn.classList.add('btn', 'btn-primary', 'ml-3', 'mb-1');
+  submitNameBtn.innerText = 'Submit';
+  container.appendChild(submitNameBtn);
+  
+  submitNameBtn.addEventListener('click', function() {
+
+    var scoreTotal = buildScoreObj(nameInput.value, score);
+    
+    scores.push(scoreTotal);
+    saveToLocalStorage(scores);
+    leaderBoard();
+  })
+}
+// End Add Name To Leaderboard Section -----------------------------------
+
+// Local Storage Section -------------------------------------------------
+function readFromLocalStorage() {
+  // console.info('Retrieveing from local storage');
+  var localStoreScores = JSON.parse(localStorage.getItem('scores'));
+  
+  if (localStoreScores) {
+    scores.length = 0
+    scores.push(...localStoreScores);
+  }
+}
+
+function saveToLocalStorage(scoresArray) {
+  // console.info('Saving scores to local storage');
+  localStorage.setItem('scores', JSON.stringify(scoresArray));
+}
+// End Local Storage Section ---------------------------------------------
+
+// Check Quiz Answers Section --------------------------------------------
 function checkAnswers(e) {
-  // Grab Response
+
   var selectedAnswerButton = e.target;
 
   if (selectedAnswerButton.value === 'true') {
     // Add/Remove Classes
     answerResult.classList.remove('hide');
     answerResult.classList.add('h4');
-
-    nextQuestionBtn.classList.remove('hide');
-
-    // Button Styles
-    nextQuestionBtn.style.margin = '-40px';
 
     // Increment numberCorrect
     numberCorrect += 1;
@@ -186,10 +251,6 @@ function checkAnswers(e) {
     answerResult.classList.remove('hide');
     answerResult.classList.add('h4');
 
-    // Increment numberWrong
-    numberWrong += 1;
-    console.log('numberWrong:', numberWrong)
-
     // subtract 10 seconds from quiz timer for wrong answer
     quizTimerLength -= 10;
 
@@ -200,16 +261,15 @@ function checkAnswers(e) {
     getQuestion();
   }
 }
+// End Check Quiz Answers Section ----------------------------------------
 
+// Populate Quiz Answers Section -----------------------------------------
 function getQuestion() {
   // check for Quiz Length
-  if (questionNum <= quizLength) {
+  if (questionNum < quizLength) {
     // Set up Question Area w/ question
     questionArea.classList.add('h4', 'my-5');
     questionArea.innerText = questions[questionNum].text;
-
-    // Add/Remove Classes
-    nextQuestionBtn.classList.add('hide');
 
     // Set up Answers Area
     createAnswersDiv.setAttribute('id', 'answersArea');
@@ -236,23 +296,15 @@ function getQuestion() {
 
     // Increment to next question
     questionNum += 1;
+
+  } else {
+    quizTimerLength = 0;
+    enterNameIntoLeaderboard();
   }
 }
+// End Populate Quiz Answers Section -------------------------------------
 
-function nextQuestion() {
-  // Grab Answer Area
-  var answerArea = document.getElementById('answersArea');
-
-  // Remove previous answers
-  answerArea.innerHTML = '';
-
-  // Hide answer result
-  // answerResult.classList.add('hide');
-
-  // Get next question
-  // getQuestion();
-}
-
+// Start Quiz Answers Section --------------------------------------------
 function startQuiz() {
   // Add/Remove classes
   startQuizBtn.classList.add('hide');
@@ -260,16 +312,15 @@ function startQuiz() {
   var instructions = document.querySelector('.instructions');
   instructions.classList.add('hide');
 
-
   // Start Timer
   startTimer();
 
   // Get Question
   getQuestion();
 }
+// End Quiz Answers Section ----------------------------------------------
 
-// Event Listeners
+// Event Listeners Section -----------------------------------------------
 startQuizBtn.addEventListener('click', startQuiz);
-
-var viewLeaderBoardBtn = document.querySelector('.viewLeaderboard');
 viewLeaderBoardBtn.addEventListener('click', leaderBoard);
+// End Event Listeners Section -------------------------------------------
